c0ea86b318f78a7a40d3ae896aa4eff1a6d3f6c7
Marat <yanbaev.marat2004@mail.ru>
2024-05-11 18:16:19 +0500

    Добавьте файлы проекта.

---
+++ Visual_editor/VE_Form.cpp
@@ -0,0 +1,12 @@
+#include "VE_Form.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+int main(array<String^>^ args) {
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::EnableVisualStyles();
+	Visualeditor::VE_Form form;
+	Application::Run(% form);
+}
